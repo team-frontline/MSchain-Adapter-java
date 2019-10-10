a0d77ec91bc261fc71d011e8d12b1bde953e9518
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2019-10-10 17:42:23 +0530

    Changes in CreateChannel class

--- src/main/java/network/CreateChannel.java
+++ src/main/java/network/CreateChannel.java
@@ -1,4 +1,19 @@
 package network;
 
+import org.hyperledger.fabric.sdk.security.CryptoSuite;
+import user.UserContext;
+import util.Util;
+
 public class CreateChannel {
+
+    public static void main(String[] args) {
+        try {
+            CryptoSuite.Factory.getCryptoSuite();
+            Util.cleanUp();
+            UserContext admin1 = new UserContext();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,4 +1,30 @@
 package util;
 
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Util {
+
+    public static void cleanUp() {
+        String directoryPath = "users";
+        File directory = new File(directoryPath);
+        deleteDirectory(directory);
+    }
+
+    public static boolean deleteDirectory(File dir) {
+        if (dir.isDirectory()) {
+            File[] children = dir.listFiles();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDirectory(children[i]);
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+
+        // either file or an empty directory
+        Logger.getLogger(Util.class.getName()).log(Level.INFO, "Deleting - " + dir.getName());
+        return dir.delete();
+    }
 }
